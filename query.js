e6cd51e6881e9c47b62a8dbda0ed2de1e7552d1f
Takumi <gggooo1111555@gmail.com>
2020-04-14 22:03:48 +0900

    sCommit

--- query.js
+++ query.js
@@ -1,6 +1,6 @@
-$(function(){
-    $('.f-p').hide().fadeIn(3000); 
-  });///ロード中ブラウザ
+// $(function(){
+//     $('.f-p').hide().fadeIn(3000); 
+//   });///ロード中ブラウザ
 
   $(window).on('load', function() {
     $('.loa').hide();
@@ -13,14 +13,14 @@ $(function(){
 
 
 
-  $(window).scroll(function (){
-	$("section").each(function(){
-	  let position = $(this).offset().top; //ページの一番上から要素までの距離を取得
-	  let scroll = $(window).scrollTop(); //スクロールの位置を取得
-	  let windowHeight = $(window).height(); //ウインドウの高さを取得
-	  if (scroll > position - windowHeight){ //スクロール位置が要素の位置を過ぎたとき
-	    $(this).addClass('active'); //クラス「active」を与える
-	  }
-	});
-});
+//   $(window).scroll(function (){
+// 	$("section").each(function(){
+// 	  let position = $(this).offset().top; //ページの一番上から要素までの距離を取得
+// 	  let scroll = $(window).scrollTop(); //スクロールの位置を取得
+// 	  let windowHeight = $(window).height(); //ウインドウの高さを取得
+// 	  if (scroll > position - windowHeight){ //スクロール位置が要素の位置を過ぎたとき
+// 	    $(this).addClass('active'); //クラス「active」を与える
+// 	  }
+// 	});
+// });
 
